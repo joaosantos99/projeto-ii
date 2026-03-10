787428d13f761fc1745d9729a74aa378665a9a19
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-03-10 15:07:00 +0000

    feat: setup env vars

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
+import env from './env.js';
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(env.PORT, () => {
+  console.log(`Server is running on port ${env.PORT}`);
 });
