996d1d6b9d0d4bc3bfb9fb1cc7742dc5f3bd6d7e
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-03-10 16:28:34 +0000

    feat: setup sequelize and cors

--- server/src/env.js
+++ server/src/env.js
@@ -3,5 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 export default {
+  // Server
   PORT: process.env.PORT || 3000,
+
+  // Database
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import cors from "cors";
 
+import sequelize from './database/connection.js';
 import env from './env.js';
 
 const app = express();
+app.use(cors())
+
 
 app.listen(env.PORT, () => {
+  // Check if the database is connected
+  sequelize.authenticate().then(() => {
+    console.log('Connection to the database has been established successfully.');
+  }).catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
   console.log(`Server is running on port ${env.PORT}`);
 });
