b83df155dd27a3ec5bdd9879a2876034dcbd7d5c
Petúnia Dias <psofia20056@gmail.com>
2026-03-11 14:43:31 +0000

    feat: add health check endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.get('/health', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.status(200).json({status: "ok"});
+})
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
