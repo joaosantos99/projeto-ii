64186b9f6de8ff0aabe181123ff0dc13ffec9345
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-04-02 15:17:56 +0100

    feat: users & roles serializers

---
+++ server/src/serializers/RoleSerializer.js
@@ -0,0 +1,25 @@
+import BaseSerializer from "./BaseSerializer";
+
+/**
+ * Serializer for the Role model.
+ */
+class RoleSerializer extends BaseSerializer {
+
+  /**
+   * Serialize a single role.
+   * @param {Object} role - The role to serialize.
+   * @returns {Object} The serialized role.
+   */
+  static serializeOne(role) {
+    this.baseValidation(role);
+
+    return {
+      id: role.id,
+      name: role.name,
+      permissions: role.permissions ?? JSON.parse(role.permissions_dump ?? '[]'),
+      createdAt: new Date(role.created_at).toISOString(),
+    };
+  }
+}
+
+export default RoleSerializer;
