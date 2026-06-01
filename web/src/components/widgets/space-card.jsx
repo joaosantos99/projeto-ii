61cbf410059e2f4946d14ffad570b4c06208e864
João Santos <68588265+joaosantos99@users.noreply.github.com>
2026-06-01 14:06:05 +0100

    fix: lp cards href to public space page

--- web/src/components/widgets/space-card.jsx
+++ web/src/components/widgets/space-card.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "#/components/ui/card"
 import { Badge } from "#/components/ui/badge.jsx"
 import { Button } from "#/components/ui/button.jsx"
@@ -6,23 +7,24 @@ import { Warning } from "@phosphor-icons/react"
 export function SpaceCard({ space }) {
     return (
         <Card className="pt-0 overflow-hidden">
-            <img src={space.image} alt={space.name} className="w-full h-48 object-cover"/>
-            <CardHeader >
-                <div className="flex items-center justify-between">
-                    <div>
-                        <CardTitle>{space.name}</CardTitle>
-                        <CardDescription>{space.city}</CardDescription>
+            <Link to="/space-public-page" className="block">
+                <img src={space.image} alt={space.name} className="w-full h-48 object-cover"/>
+                <CardHeader className="pt-6">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <CardTitle>{space.name}</CardTitle>
+                            <CardDescription>{space.city}</CardDescription>
+                        </div>
+                        <Badge variant={
+                            space.status === "Normal" ? "secondary" :
+                            space.status === "Atenção" ? "warning" :
+                            "destructive"
+                        }>
+                            {space.status ?? "Normal"}
+                        </Badge>
                     </div>
-                    <Badge variant={
-                        space.status === "Normal" ? "secondary" :
-                        space.status === "Atenção" ? "warning" :
-                        "destructive"
-                    }>
-                        {space.status ?? "Normal"}
-                    </Badge>
-                </div>
-
-            </CardHeader>
+                </CardHeader>
+            </Link>
             <CardContent className="flex flex-col gap-4">
                 <div className="grid grid-cols-4 gap-2">
                     <div className="border border-border p-2 flex flex-col gap-1 justify-between">
